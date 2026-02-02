4ab0896dae551c5bd9fd4a84d1a1b8ac11f25f6f
Jeremy Kihiro Mwaura <jeremy.mwaura@student.moringaschool.com>
2026-02-02 17:12:14 +0300

    getting user inputs

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 document.getElementById("akanForm").addEventListener("submit", function(e) {
     e.preventDefault();
+
+    const day = parseInt(document.getElementById("day").value);
+    const month = parseInt(document.getElementById("month").value);
+    const year = parseInt(document.getElementById("year").value);
+    const gender = document.getElementById("gender").value;
+    const result = document.getElementById("result");
